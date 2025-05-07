a0e17daa3bc3506ab946544b263e0282376f8020
Amarusinggithub <loveamar285@gmail.com>
2025-05-07 16:08:10 -0500

    changes

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -3,9 +3,11 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],envDir:path.resolve(__dirname, "../"),
+    envPrefix: "VITE_",
   test: {
     globals: true,
     environment: "jsdom",
