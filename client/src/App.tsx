90dc301ccdea43d9503befff4039851af95799a1
Amarusinggithub <loveamar285@gmail.com>
2025-12-30 13:35:52 -0500

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
 import { TagProvider } from './hooks/use-mutate-tag.tsx';
-import { PageProvider } from './hooks/use-page.tsx';
-
 import ErrorFallback from './pages/error.tsx';
 import AppRoutes from './routes/app-routes.tsx';
 
@@ -16,11 +14,9 @@ export default function App() {
     return (
             <ErrorBoundary FallbackComponent={ErrorFallback}>
                 <QueryClientProvider client={queryClient}>
-                        <PageProvider>
                             <TagProvider>
                                 <AppRoutes />
                             </TagProvider>
-                        </PageProvider>
                 </QueryClientProvider>
             </ErrorBoundary>
     );
