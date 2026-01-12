8d83bbc3b83896c71215eec6da11e792e1e046c2
Amar campbell <100084611+Amarusinggithub@users.noreply.github.com>
2026-01-12 15:48:40 -0500

    fix!: update test that was causing api ci workflow to fail and updated pnpm lock file
    * fix!: update test that was causing api ci workflow to fail and updated pnpm lock file
    
    * fix!: update test that was causing api ci workflow to fail and updated pnpm lock file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import '@liveblocks/react-ui/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
-import { TagProvider } from './hooks/use-tag.tsx';
 import ErrorFallback from './pages/error.tsx';
 import AppRoutes from './routes/app-routes.tsx';
 
@@ -19,9 +18,7 @@ export default function App() {
 	return (
 		<ErrorBoundary FallbackComponent={ErrorFallback}>
 			<QueryClientProvider client={queryClient}>
-				<TagProvider>
 					<AppRoutes />
-				</TagProvider>
 			</QueryClientProvider>
 		</ErrorBoundary>
 	);
